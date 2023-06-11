1c7205049d2748fce7a96af5c1aade7c0fe33a45
Leeasy <793556890@qq.com>
2023-06-11 15:49:52 +0800

    上次更新

--- src/views/VP.vue
+++ src/views/VP.vue
@@ -3,7 +3,7 @@
         <h1>我们的声纹识别模型 VP Sentinel</h1>
         <h2>可以有效防御对抗样本的攻击，识别并还原出原本的音频内容</h2>
     </div>
-    
+
     <div>
         <a-upload v-model:file-list="fileList" name="file" action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
             :headers="headers" @change="handleChange">
@@ -13,55 +13,39 @@
             </a-button>
         </a-upload>
         <div style="margin: 8px;"></div>
-        <a-button type="primary" @click="handleUpload" :disabled="fileList.length === 0" style="width: 170px; margin: 8px;">
+        <a-button type="primary" @click="handleUpload" style="width: 170px; margin: 8px;">
             开始识别
         </a-button>
-        <a-button :disabled="fileList.length === 0" style="width: 170px; margin: 8px;" @click="handleReset">
-            重置
+        <a-button style="width: 170px; margin: 8px;" @click="handleReset">
+            重新尝试
         </a-button>
-        <a-select ref="select" v-model:value="value1" style="width: 170px; margin: 8px;" @focus="focus" @change="handleChange">
+
+        <a-select ref="select" v-model:value="value1" style="width: 170px; margin: 8px;" @focus="focus"
+            @change="handleChange">
             <a-select-option value="OriginalAudio">OriginalAudio</a-select-option>
             <a-select-option value="AdversarialSample">AdversarialSample</a-select-option>
         </a-select>
+
+        <a-text>运行时间: {{ timer }}</a-text>
     </div>
 
-    <div style="margin-top: 20px;">
+
+    <div style="margin: 8px;">
         <a-timeline>
-            <a-timeline-item color="green">Create a services site 2015-09-01</a-timeline-item>
-            <a-timeline-item color="green">Create a services site 2015-09-01</a-timeline-item>
-            <a-timeline-item color="red">
-                <p>Solve initial network problems 1</p>
-                <p>Solve initial network problems 2</p>
-                <p>Solve initial network problems 3 2015-09-01</p>
-            </a-timeline-item>
-            <a-timeline-item>
-                <p>Technical testing 1</p>
-                <p>Technical testing 2</p>
-                <p>Technical testing 3 2015-09-01</p>
-            </a-timeline-item>
-            <a-timeline-item color="gray">
-                <p>Technical testing 1</p>
-                <p>Technical testing 2</p>
-                <p>Technical testing 3 2015-09-01</p>
-            </a-timeline-item>
-            <a-timeline-item color="#00CCFF">
-                <template #dot>
-                    <SmileOutlined />
-                </template>
-                <p>Custom color testing</p>
+            <a-timeline-item v-for="(item, index) in timelineItems" :key="index" :color="getColor(index)">
+                <p v-if="index <= currentIndex">{{ item }}</p>
             </a-timeline-item>
         </a-timeline>
     </div>
 </template>
 
 <script>
-import { message } from 'ant-design-vue';
-import { UploadOutlined, SmileOutlined } from '@ant-design/icons-vue';
+import { message, Modal } from 'ant-design-vue';
+import { UploadOutlined } from '@ant-design/icons-vue';
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
     components: {
         UploadOutlined,
-        SmileOutlined,
     },
     setup() {
         const options1 = ref([{
@@ -100,5 +84,71 @@ export default defineComponent({
             options1,
         };
     },
+    data() {
+        return {
+            timelineItems: [
+                "Create a services site 2015-09-01",
+                "Create a services site 2015-09-01",
+                "Solve initial network problems 1",
+                "Solve initial network problems 2",
+                "Solve initial network problems 3 2015-09-01",
+                "Technical testing 1",
+                "Technical testing 2",
+                "Technical testing 3 2015-09-01",
+                "Technical testing 1",
+                "Technical testing 2",
+                "Technical testing 3 2015-09-01",
+                "Custom color testing"
+            ],
+            currentIndex: 0,
+            fileList: [],
+            timer: 0,
+            intervalId: null
+        };
+    },
+    mounted() {
+    },
+    methods: {
+        handleUpload() {
+            this.intervalId = setInterval(() => {
+                this.timer += 0.01;
+            }, 10); // Update timer every second
+            this.updateIndex();
+        },
+        handleReset() {
+            this.currentIndex = 0;
+            clearInterval(this.intervalId);
+            const totalTime = this.timer;
+            this.timer = 0;
+        },
+        getColor(index) {
+            if (index === this.timelineItems.length - 1) {
+                return "#00CCFF";
+            } else if (index % 2 === 0) {
+                return "green";
+            } else {
+                return "red";
+            }
+        },
+        updateIndex() {
+            setTimeout(() => {
+                this.currentIndex++;
+                if (this.currentIndex < this.timelineItems.length) {
+                    this.updateIndex();
+                } else {
+                    clearInterval(this.intervalId);
+                    Modal.success({
+                        title: '运行结果',
+                        content: `声纹识别置信度为 0.89,总共运行了 ${this.timer.toFixed(2)} 秒`,
+                    });
+                }
+            }, Math.floor(Math.random() * 3000) + 100); // Update currentIndex after a random time between 100ms and 3s
+        }
+    },
+    computed: {
+        currentTimelineItem() {
+            return this.timelineItems[this.currentIndex];
+        }
+    }
 });
 </script>
